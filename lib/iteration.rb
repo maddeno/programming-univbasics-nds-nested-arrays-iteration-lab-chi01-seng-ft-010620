afbedf7f369e3e07e9416af0a5ca8738dab6654a
Olivia Madden <madden.livi@gmail.com>
2019-12-27 23:26:43 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -36,10 +36,11 @@ def total_even_pairs(src)
   total = 0 
   row_index = 0 
   while row_index < src.count do 
-    if src[row_index][0] % 2 ==0 
-      && src[row_index][2] % 2 == 0 
-      src[row_index].sum 
-      
+    if src[row_index][0] % 2 ==0 && 
+      src[row_index][1] % 2 == 0 
+      total += src[row_index][0] + src[row_index][1]
+    end
+    row_index += 1   
   end
   total
 end
